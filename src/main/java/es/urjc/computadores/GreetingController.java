41a47c712a823d28b971bd6e131edf919731a7a7
Miguel Santiago <na>
2020-02-21 13:49:27 +0100

    # WARNING: head commit changed in the meantime
    Implementados controladores de pedido y producto

--- src/main/java/es/urjc/computadores/GreetingController.java
+++ src/main/java/es/urjc/computadores/GreetingController.java
@@ -44,83 +44,9 @@ public class GreetingController implements CommandLineRunner{
 	public String greeting(Model model) {
 		return "greeting_template";
 	}
-	
-	
-	
-	@GetMapping("/inputproducto")
-	public String insertarProducto(Model model, @RequestParam String precio, String categoria, String descripcion, String usuario) {
 
-		List<Usuario> p = usuarioRepo.findByNombre(usuario);		
-		Producto p1 = new Producto(Double.parseDouble(precio),categoria,descripcion,p.get(0));		
-		productoRepo.save(p1);
-		return "subirproducto";
-	}
-	
-	
-	@GetMapping("producto/inputpedido/{productid}")
-	public String insertarPedido(Model model, @PathVariable Long productid, @RequestParam String origen,String destino, String remitente) {
-		Usuario user = usuarioRepo.findByNombre(remitente).get(0);
-		Producto p = productoRepo.findById(productid).get();
-		Pedido pedido = new Pedido(p,origen,destino,user);
-		pedidoRepo.save(pedido);
-		model.addAttribute("producto", p);
-		return "producto";
-	}
-	
-	
-	@GetMapping("/producto/{num}")
-	public String verProducto(Model model, @PathVariable Long num) {
-		
-		
-		Producto elegido = productoRepo.findById(num).get();
-
-		model.addAttribute("producto", elegido);
-
-		return "producto";
-	}
 	
 
 	
 	
-	
-	
-	
-	
-	//TODO: separar en varios controladores
-	@GetMapping("/peticion")
-	public String devolverLista(Model model, @RequestParam String peticion) { 
-		if(peticion.equals("usuarios")) {
-			
-			
-			
-			
-			// TODO: quita la parte del producto!!!
-			
-			
-			
-			
-			
-		} else if (peticion.equals("productos")) {
-			List<Producto> lista = productoRepo.findAll();
-			model.addAttribute("datos", lista);
-			return "productos";
-		}
-		return "listadevuelta";
-	}
-	@GetMapping("/subirproducto")
-	public String subirProducto(Model model) {
-		return "subirproducto";
-	}
-	
-
-	
-	@GetMapping("/{id}/gestionenvios")
-	public String gestionenvios(Model model,@PathVariable Long id) {
-		Usuario u = usuarioRepo.findById(id).get();
-		model.addAttribute("vendidos", u.getProductosVendidos());
-		model.addAttribute("comprados", u.getProductosComprados());
-		model.addAttribute("userid", u.getId());
-		
-		return "gestionenvios";
-	}
 }
