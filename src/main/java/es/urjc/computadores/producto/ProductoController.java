9459bde820515300c4467a2d8210dfbeb9308930
Diego Díaz Pérez <diego.diaz@koronstudios.com>
2020-03-24 19:54:03 +0100

    Bug cuando entrabas a un producto sin loggearte

--- src/main/java/es/urjc/computadores/producto/ProductoController.java
+++ src/main/java/es/urjc/computadores/producto/ProductoController.java
@@ -55,11 +55,18 @@ public class ProductoController implements CommandLineRunner {
 
 	@GetMapping("/producto/{num}")
 	public String verProducto(Model model, @PathVariable Long num) {
-		Producto elegido = productoRepo.findById(num).get();
-		model.addAttribute("producto", elegido);
-		model.addAttribute("user", usuario.getLoggedUser());
-		model.addAttribute("userid", usuario.getLoggedUser().getId());
-		return "producto";
+		
+		if(usuario.getLoggedUser() != null) {
+			Producto elegido = productoRepo.findById(num).get();
+			model.addAttribute("producto", elegido);
+			model.addAttribute("user", usuario.getLoggedUser());
+			model.addAttribute("userid", usuario.getLoggedUser().getId());
+			return "producto";
+		}
+		else {
+			return "SignUp";
+		}
+		
 	}
 
 	@GetMapping("/subirproducto")
