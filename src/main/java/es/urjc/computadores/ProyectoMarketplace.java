b34011f23871c400237b634e586fec663b39bace
Diego Díaz Pérez <diego.diaz@koronstudios.com>
2020-04-25 19:07:20 +0200

    Configurado Hazelcast - falta probar

--- src/main/java/es/urjc/computadores/ProyectoMarketplace.java
+++ src/main/java/es/urjc/computadores/ProyectoMarketplace.java
@@ -1,13 +1,30 @@
 package es.urjc.computadores;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class ProyectoMarketplace {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoMarketplace.class, args);
+	}
+	
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+		return config;
 	}
 
 }
